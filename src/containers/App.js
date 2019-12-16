1ff387886c54ed92211957a1ff4d6a060a0b84ef
mahmudin muttaqin <claxmanx@gmail.com>
2019-12-16 14:50:24 +0700

    tambah navbar

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,8 +8,26 @@ class App extends Component {
   render() {
     return (
       <Router>
-      	<Link to="/">Home</Link> &nbsp;
-      	<Link to="books">Books</Link>
+
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container">
+            <Link to="/" className="navbar-brand">React Redux CRUD</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="books">Books</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
 
         <Switch>
         	<Route exact path="/" component={Home}/>
--- src/containers/pages/Book/BookListPage.js
+++ src/containers/pages/Book/BookListPage.js
@@ -12,41 +12,56 @@ class BookListPage extends Component {
 	handleDelete = (id) => {
 		console.log(id)
 		this.props.deleteBooks(id)
-			.then((res) => {
-				console.log(res)
-				this.props.getBooks()
-			}, (err) => {
-				console.log(err.response)
-			})
+			// .then((res) => {
+			// 	console.log(res)
+			// 	this.props.getBooks()
+			// }, (err) => {
+			// 	console.log(err.response)
+			// })
 	}
 
 	render() {
+
+
+		const loading = (
+			<div className="row mt-3">
+				<p>Loading...</p>
+			</div>
+		)
+
+		const books = ( 
+			<div className="row mt-3">
+				{this.props.book.data.map((item, key) => 
+					<div className="col-md-4" key={item.id}>
+						<div className="card">
+							<div className="card-header">
+								Buku {item.name}
+								<br/>
+								<hr/>
+								<Link to={`/books/${item.id}/edit`} className="btn btn-sm btn-primary">EDIT</Link> &nbsp;
+								<button onClick={() => this.handleDelete(item.id)} className="btn btn-sm btn-danger">DELETE</button>
+							</div>
+							<div className="card-body">
+								Author {item.author} <br/>
+								Category {item.category}
+							</div>
+						</div>
+						<br/>
+					</div>
+				)}
+			</div>				
+		)
+	
+
 		return(
 			<Fragment>
-				<div className="container">
-					<div className="row">
-						<Link to="/books/create" className="btn btn-primary float-right">CREATE NEW BOOK</Link>
-					</div>
+				<div className="container mt-5">
 
-					<div className="row mt-3">
-						{this.props.book.data.map((item, key) => 
-							<div className="col-md-4" key={item.id}>
-								<div className="card">
-									<div className="card-header">
-										Buku {item.name}
-										<br/>
-										<hr/>
-										<Link to={`/books/${item.id}/edit`} className="btn btn-sm btn-primary">EDIT</Link> &nbsp;
-										<button onClick={() => this.handleDelete(item.id)} className="btn btn-sm btn-danger">DELETE</button>
-									</div>
-									<div className="card-body">
-										Author {item.author} <br/>
-										Category {item.category}
-									</div>
-								</div>
-							</div>
-						)}
-					</div>
+					<Link to="/books/create" className="btn btn-primary">CREATE NEW BOOK</Link>
+					<div className="clearfix"></div>
+
+					{ this.props.book.data.length > 0 ? books : loading }
+
 				</div>
 			</Fragment>
 		)
